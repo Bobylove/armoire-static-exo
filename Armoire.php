cbe2db17af03f4e1f6d4ba4e65e4115606846f38
Fred <delon.frederic.pro.job@gmail.com>
2017-02-03 09:32:40 +0100

    add link semantic ui

--- Armoire.php
+++ Armoire.php
@@ -1,44 +1,37 @@
 <?php 
 require_once 'vendor/j4mie/idiorm/idiorm.php';
 
-ORM::configure('mysql:host=localhost;dbname=mon_armoire');
-ORM::configure('username','root');
-ORM::configure('password','simplonco');
-
-
-
 class Armoire {
 
+	public $db;
+
 	public function __construct(){
-		$armoire = ORM::for_table('mes_chaussettes')->find_many();
+
+		ORM::configure('mysql:host=localhost;dbname=mon_armoire');
+		ORM::configure('username','root');
+		ORM::configure('password','simplonco');
+
+		$this->db = ORM::for_table('mes_chaussettes');
+
 	}
 
 	public function getInstance(){
-
+		return $this->db;
 	}
 
-	public static function all($limit,$offset){
-
-
+	public static function all($limit=10, $offset=0){
+		$tab = new self();	
+		return $tab->getInstance()
+		->limit($limit)
+		->offset($offset)
+		->find_many();
 	}
 
 	public static function get($id){
+		$tabId = new self();
+		return $tabId->getInstance()->find_one($id);
 
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,46 @@
+<?php require 'Armoire.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Tableau des 50 premières entrées</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.7/semantic.min.css"/>
 </head>
 <body>
-	
+	<h1>
+		<a href="index.php">Home</a>
+		<div class="ui table">
+			<table class="ui table">
+				<tr>
+					<th>---id---</th>
+					<th>pointure</th>
+					<th>couleur</th>
+					<th>temp_lavage</th>
+					<th>date_lavage</th>
+				</tr>
+				<?php
+				$tab = Armoire::all(10,0);
+
+
+				foreach ($tab as $value) {
+					?>
+					<tr>
+						<td><a href="index.php?id=<?php echo $value->id ?>"><?php echo $value->id; ?></a></td>
+						<td><?php echo $value->pointure; ?></td>
+						<td><?php echo $value->couleur; ?></td>
+						<td><?php echo $value->temp_lavage; ?>°c</td>
+						<td><?php echo $value->date_lavage; ?></td>
+
+					</tr>
+
+					<?php 
+				}
+				?>
+			</table>
+		</div>
+		<br>
+		<br>
+		<br>
+	</h1>
 </body>
 </html>
